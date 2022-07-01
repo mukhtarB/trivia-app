1197899d499018cf46f89281f9cbba4e8389ccae
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-07-01 22:48:14 +0100

    set up tests and test_categories

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -6,6 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+from decouple import config
+
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -15,7 +17,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format(config('DB_USER'), config('DB_PASSWORD'), 'localhost:5432',
+                                                       self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,15 +27,40 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+        
+        self.new_question = {
+            'question': 'Is Shrodingers cat dead or alive',
+            'answer': "It's in a constant state of dead and alive",
+            'difficulty': 5,
+            'category': 1,
+        }
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
     """
     TODO
-    Write at least one test for each test for successful operation and for expected errors.
+    Write at least one test for each test for successful
+    operation and for expected errors.
     """
+
+    def test_get_categories(self):
+        """Test that it returns all categories."""
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        res_data = {
+                "1": "Science",
+                "2": "Art",
+                "3": "Geography",
+                "4": "History",
+                "5": "Entertainment",
+                "6": "Sports"
+            }
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data, res_data)
 
 
 # Make the tests conveniently executable
